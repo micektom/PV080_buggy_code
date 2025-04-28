b9aedc256d7ed89cbd32fa871fa1a8de0529f850
Tomáš Tomíček <tomastomicek03@gmail.com>
2025-04-28 12:48:06 +0200

    fix some code issues

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 import os
 import yaml
 import flask
@@ -31,7 +31,7 @@ def fetch_website(urllib_version, url):
     try: 
         http = urllib.PoolManager()
         r = http.request('GET', url)
-    except:
+    except Exception:
         print('Exception')
 
 
@@ -40,10 +40,12 @@ def load_yaml(filename):
     deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
     
+
 def authenticate(password):
     # Assert that the password is correct
     assert password == "Iloveyou", "Invalid password!"
     print("Successfully authenticated!")
+
 
 if __name__ == '__main__':
     print("Vulnerabilities:")
@@ -52,7 +54,7 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability:")
     print("4. Use of assert statements vulnerability:")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
+    if choice == "1":
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
@@ -64,4 +66,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
